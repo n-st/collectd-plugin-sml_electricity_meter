9f6c210f7328715054324af3eb99842af8df126b
Nils Steinger <git@n-st.de>
2016-05-09 01:37:01 +0200

    Implemented SML data dispatch to Collectd

--- src/de/voidptr/collectd_plugins/SMLReceiver.java
+++ src/de/voidptr/collectd_plugins/SMLReceiver.java
@@ -1,15 +1,31 @@
 package de.voidptr.collectd_plugins;
 
-import gnu.io.PortInUseException;
-import gnu.io.UnsupportedCommOperationException;
 import org.collectd.api.*;
 import org.openmuc.jsml.structures.*;
 import org.openmuc.jsml.tl.SML_SerialReceiver;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SMLReceiver implements CollectdConfigInterface, CollectdInitInterface, CollectdShutdownInterface {
+
+    /*
+     All new types must be manually added to Collectd's type list (to path to which is specified in collectd.conf).
+     For most values from the SML energy meter, a simple positive-only gauge should suffice, e.g.
+     watts    value:GAUGE:0:U
+     Since Collectd will reject values that reference an unknown data type, we need to keep a list of units for
+     which we can pass on data to Collectd.
+     */
+    private static final Map<Integer, String> collectdTypeForSMLUnit;
+    static {
+        Map<Integer, String> temporaryMap = new HashMap<>();
+        temporaryMap.put(SML_Unit.WATT, "watts");
+        temporaryMap.put(SML_Unit.WATT_HOUR, "watt_hours");
+        collectdTypeForSMLUnit = Collections.unmodifiableMap(temporaryMap);
+    }
 
     private final SML_SerialReceiver receiver = new SML_SerialReceiver();
     private String serialPort = null;
@@ -74,7 +90,7 @@ public class SMLReceiver implements CollectdConfigInterface, CollectdInitInterfa
 
     private class ValueDispatcherThread implements Runnable {
 
-        private String serverIdToString(OctetString serverId) {
+        private String octetStringToHexString(OctetString serverId) {
             StringBuilder result = new StringBuilder();
 
             for (byte b : serverId.getOctetString()) {
@@ -86,27 +102,32 @@ public class SMLReceiver implements CollectdConfigInterface, CollectdInitInterfa
             return result.toString();
         }
 
-        @Override
-        public void run() {
+        private int dispatchValue(String serverId, String obisCode, String unitName, double value) {
             Collectd.logInfo("Dispatching value...");
+
             ValueList valueList = new ValueList();
+
             // Commit cadc1d5 made it possible to use to hostname from collectd.conf for the Java plugin.
             // Unfortunately, that commit is only included in v5.5.0 and higher, while Ubuntu 14.04 ships v5.4.0.
             valueList.setHost("ubuntu.has.an.outdated.collectd.version");
             valueList.setPlugin("smlreceiver");
-            valueList.setPluginInstance("09:01:45:4D:48:00:00:4E:B9:A6");
-            valueList.setType("power");
-            valueList.setTypeInstance("1:0:1:8:0:255");
+            valueList.setPluginInstance(serverId);
+            valueList.setType(unitName);
+            valueList.setTypeInstance(obisCode);
             valueList.setInterval(1<<30);
-            valueList.addValue(42);
+            valueList.addValue(value);
 
-            Collectd.dispatchValues(valueList);
+            return Collectd.dispatchValues(valueList);
+        }
 
+        @Override
+        public void run() {
             while (true) {
 
                 SML_File smlFile = null;
                 try {
                     smlFile = receiver.getSMLFile();
+
                 } catch (IOException e) {
                     Collectd.logError("Error while trying to read SML data: " + e.getMessage());
                     return;
@@ -114,60 +135,49 @@ public class SMLReceiver implements CollectdConfigInterface, CollectdInitInterfa
 
                 List<SML_Message> smlMessages = smlFile.getMessages();
 
-                for (int i = 0; i < smlMessages.size(); i++) {
-                    SML_Message sml_message = smlMessages.get(i);
+                for (SML_Message sml_message : smlMessages) {
                     int tag = sml_message.getMessageBody().getTag().getVal();
                     if (tag == SML_MessageBody.GetListResponse) {
                         SML_GetListRes resp = (SML_GetListRes) sml_message.getMessageBody().getChoice();
                         SML_List smlList = resp.getValList();
 
-                        System.out.print("Server-ID: ");
-                        System.out.println(serverIdToString(resp.getServerId()));
+                        String serverIdString = octetStringToHexString(resp.getServerId());
 
                         SML_ListEntry[] list = smlList.getValListEntry();
 
                         for (SML_ListEntry entry : list) {
                             int unit = entry.getUnit().getVal();
-                            String unitName = null;
-                            // Only handle entries with meaningful units
-                            switch (unit) {
-                                case SML_Unit.WATT:
-                                    unitName = "W";
-                                    break;
-                                case SML_Unit.WATT_HOUR:
-                                    unitName = "Wh";
-                                    break;
+                            // Only handle entries with units for which we have Collectd type definitions
+                            if (! collectdTypeForSMLUnit.containsKey(unit)) {
+                                continue;
                             }
-                            if (unitName != null) {
-                                long numericalValue;
+                            String unitName = collectdTypeForSMLUnit.get(unit);
+                            long numericalValue;
 
-                                SML_Value value = entry.getValue();
-                                ASNObject obj = value.getChoice();
+                            SML_Value value = entry.getValue();
+                            ASNObject obj = value.getChoice();
 
-                                if (obj.getClass().equals(Integer32.class)) {
-                                    Integer32 val = (Integer32) obj;
-                                    numericalValue = val.getVal();
-                                } else if (obj.getClass().equals(Integer64.class)) {
-                                    Integer64 val = (Integer64) obj;
-                                    numericalValue = val.getVal();
-                                } else {
-                                    System.out.println("Got non-numerical value for an energy measurement. Skipping.");
-                                    continue;
-                                }
-
-                                byte objNameBytes[] = entry.getObjName().getOctetString();
-                                // We need to force Java to treat the bytes as unsigned integers by AND-ing them with 0xFF
-                                System.out.printf("%d-%d:%d.%d.%d*%d = %.1f %s\n",
-                                        0xFF & objNameBytes[0], 0xFF & objNameBytes[1], 0xFF & objNameBytes[2],
-                                        0xFF & objNameBytes[3], 0xFF & objNameBytes[4], 0xFF & objNameBytes[5],
-                                        numericalValue / 10.0,
-                                        unitName);
+                            if (obj.getClass().equals(Integer32.class)) {
+                                Integer32 val = (Integer32) obj;
+                                numericalValue = val.getVal();
+                            } else if (obj.getClass().equals(Integer64.class)) {
+                                Integer64 val = (Integer64) obj;
+                                numericalValue = val.getVal();
+                            } else {
+                                System.out.println("Got non-numerical value for an energy measurement. Skipping.");
+                                continue;
                             }
+
+                            String obisCode = octetStringToHexString(entry.getObjName());
+
+                            dispatchValue(serverIdString, obisCode, unitName, numericalValue / 10.0);
                         }
                     }
                 }
 
                 System.out.println();
+
+
 
             }
         }
