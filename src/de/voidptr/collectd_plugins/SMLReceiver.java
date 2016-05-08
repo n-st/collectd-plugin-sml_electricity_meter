ba36538dd877612efaa31fa4702b54d4ebce8cbd
Nils Steinger <git@n-st.de>
2016-05-08 17:00:03 +0200

    Collectd Java plugin skeleton

---
+++ src/de/voidptr/collectd_plugins/SMLReceiver.java
@@ -0,0 +1,9 @@
+package de.voidptr.collectd_plugins;
+
+import org.collectd.api.*;
+
+public class SMLReceiver {
+    public SMLReceiver() {
+        Collectd.logError("That wasn't chicken!");
+    }
+}
